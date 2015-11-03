27dcd97fa45c2fc3b77fe03b884643e2377b0376
neo1218 <neo1218@yeah.net>
2015-11-03 18:41:50 +0800

    add :class: MuxiRequest

---
+++ muxi/muxi.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+"""
+	木muxi犀
+	~~~~~~~~
+
+		a python web framework ~ on top of flask
+		baseon werkzeug ~ jinja ~ mana
+
+		:copyright: (c) 2015 by neo1218(朱承浩)
+		:license: MIT, see LICENSE for more details.
+"""
+
+# models import
+# basic models
+
+# werkzeug
+from werkzeug import Request
+
+# jinja
+
+# mana
+
+# models not used in this file but are exported as public interface
+
+
+class MuxiRequest(Request):
+	"""this request obj used by default in muxi"""
+
+	def __init__(self, environ):
+		Request.__init__(self, environ)
+		self.endpoint = None
+		self.view_args = None
