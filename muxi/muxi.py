923a122f9f173d5d9c1f34548ee752cd8049efea
neo1218 <neo1218@yeah.net>
2015-11-04 09:07:36 +0800

    add class:Muxi and request obj info

--- muxi/muxi.py
+++ muxi/muxi.py
@@ -15,7 +15,7 @@
 from basedir import _basedir
 
 # werkzeug
-from werkzeug.wrappers import Request
+from werkzeug.wrappers import Request, Response
 
 # jinja
 
@@ -31,3 +31,50 @@ class MuxiRequest(Request):
 		Request.__init__(self, environ)
 		self.endpoint = None
 		self.view_args = None
+
+
+class MuxiResponse(Response):
+	"""
+	Response obj used by default in muxi
+	but set default mimetype as html
+	"""
+	default_mimetype = 'text/html'
+
+
+class _RequestGlobals(object):
+	pass
+
+
+class _RequestContext(object):
+	"""
+	request context contains all request relevant info,
+	it is created at the beginning of the request and
+	pushed to :_request_ctx_stack: and
+	removed at the end of it,
+	it will create the URL adapter and request obj for
+	the WSGI environment provided.
+	"""
+
+	def __init__(self, app, environ):
+		self.app = app
+		self.url_adapter = app.url_map.bind_to_environ(environ)
+		self.request = app.request_class(environ)
+		self.session = app.open_session(self.request)
+		# g is in request ctx
+		# and used to store anything
+		self.g = _RequestGlobals()
+		self.showes = None
+
+
+class Muxi(object):
+	"""
+	The :class~Muxi: obj implements a WSGI application and
+	automatically config the app & register view functions through
+	the name of the module or package passed.
+	"""
+
+	# the class for request obj
+	request_class = MuxiRequest
+
+	# the class for response obj
+	response_class = MuxiResponse
